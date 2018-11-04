ec1578c3aa949b89a409ab976c28bb76ed42f9d2
simiss98 <simanskas.e@gmail.com>
2018-11-05 00:59:13 +0200

    Module 1.1

---
+++ Module1.1/MOD01_1-1.3.py
@@ -0,0 +1,23 @@
+# Creating a bucket variable with integer - value (16)
+bucket=16
+type(bucket)
+#Creating a bucket variable with string - value ("Hello world") while using double quotes.
+bucket = "Hello world"
+type(bucket)
+#Creating a bucket variable with string - value ("Hello world") while using single quotes.
+bucket = 'O Dangau!'
+type(bucket)
+#displaying type of the string value, while using double quotes.
+type("Hello world")
+#displaying type of the string value, while using double quotes.
+type("12")
+#displaying type of the integer value.
+type(12)
+#displaying type of the negative integer value.
+type(-12)
+#displaying type of float.
+type(12.0)
+#displaying type of float.
+type(1.55)
+#displaying the calss and type of the variable.
+print(type(3))
