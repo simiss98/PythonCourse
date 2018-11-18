5521d50138511d4308dbb96f57603d15d2fb9c65
simiss98 <simanskas.e@gmail.com>
2018-11-19 01:30:55 +0200

    Unit1

--- UNIT_1/Module4.1/Practice_MOD04_1-6_IntroPy.py
+++ UNIT_1/Module4.1/Practice_MOD04_1-6_IntroPy.py
@@ -38,7 +38,8 @@ Blue_shirt_M = 400
 
 
 order = "Welcome to shirt world, please start your order."
-color = str(input("What color shirt you would like, White and Blue? ")).title()
+print(order)
+color = str(input("What color shirt you would like, White or Blue? ")).title()
 if color == "White" or color == "Blue":
     print("Thank you, "+color+ " is good decision")
     size = input("What size would you like, L or M: ").title()
@@ -63,3 +64,52 @@ if color == "White" or color == "Blue":
         print("Sorry we do not have size "+size)
 else:
     print("Sorry we do not have \""+color+"\" color")
+
+# [ ] create and test str_analysis()
+def str_analysis(str_input):
+    test = str_input
+    # Check if string is digits
+    # if digits: convert to int and check if greater than 99
+    if test.isdigit() and int(test) > 99:
+        print("Checking if entered string includes digits only := ", test.isdigit())
+        # if greater than 99, print a message about a "big number"
+        print("\nYour number is greater than 99")
+    elif test.isdigit() and int(test) < 99:
+        # if not greater than 99, print message about "small number"
+        print("\nYour number is smaller than 99")
+        # if not digits: check if string isalpha
+    elif test.isalpha():
+        # if isalpha print message about being all alpha
+        print("Your string includes alphabetical characters only")
+    else:
+        # if not isalpha print a message about being neither all alpha nor all digit
+        print("Your entered string is not all alpha and is not all digits")
+
+
+# [ ] create and call ticket_check()
+
+def ticket_check(section):
+    # string and expects: general, floor
+    # if section is general( or use startswith "g")
+    # if section is floor( or use starts with "f")
+    if (section.startswith("G") or section.startswith("F")) and (section == "General" or section == "Floor"):
+        print("Thank for choosing "+section+" section")
+        seat = input("Enter a seat number"+"\nFor General Section 1 - 10"+"\nFor Floor section 1 - 4 " )
+        # integer and expects: 1 - 10
+        # if seats is 1 - 10 return True
+        if seat.isdigit() and int(seat) >= 1 and int(seat) <= 10 and section == "General":
+            print("Seat " + seat + " is available")
+            # if seats is 1 - 4 return True
+        elif seat.isdigit() and int(seat) >= 1 and int(seat) <= 4 and section == "Floor":
+            print("Seat " + seat + " is available")
+        else:
+            #IF seat is outside seat range
+            print("There is no seat \""+seat+"\" in the section ",section)
+    else:
+        # IF section is not general or floor
+        print("There is no "+section+" section")
+
+
+
+
+call = ticket_check(input("Enter a section General or Floor: ").title())
